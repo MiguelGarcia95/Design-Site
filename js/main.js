77dffc9bb9fc0802525ec4cd6cae58032c8398b1
MiguelGarcia95 <mgarcia95951@gmail.com>
2019-08-28 22:37:29 -0500

    create base styling

--- js/main.js
+++ js/main.js
@@ -1,18 +1,19 @@
 var scrollAnim = new TimelineMax();
-var controller = new ScrollMagic.Controller({vertical: false});
+var controller = new ScrollMagic.Controller();
+// var controller = new ScrollMagic.Controller({vertical: false});
 
-// scrollAnim
-// // .set('.slide.one', {width: 0})
-// .to('.slide.one', 1, {width: '100vw'})
-// .to('.block-test', 10, {backgroundColor: 'red'})
+scrollAnim
+// .set('.slide', {y: '-100vh'})
+// .set('.slide.one', {width: 0})
+.to('.slide.one', 2, {y: '-=100vh'})
 
-// new ScrollMagic.Scene({
-//   triggerElement: '.scroll-trigger',
-//   // offset: 1,
-//   triggerHook: 0,
-//   duration: '100%',
-// })
-// .setTween(scrollAnim)
-// .setPin('.scroll-trigger')
-// .addIndicators()
-// .addTo(controller)
+new ScrollMagic.Scene({
+  triggerElement: '.scroll-trigger',
+  // offset: 1,
+  triggerHook: 0,
+  duration: '100%',
+})
+.setTween(scrollAnim)
+.setPin('.scroll-trigger')
+.addIndicators()
+.addTo(controller)
