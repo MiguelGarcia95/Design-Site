0abfa1f6d1600b3625156b07760daae71db021c4
MiguelGarcia95 <mgarcia95951@gmail.com>
2019-08-29 17:30:54 -0500

    add slides

--- js/main.js
+++ js/main.js
@@ -3,17 +3,20 @@ var controller = new ScrollMagic.Controller();
 // var controller = new ScrollMagic.Controller({vertical: false});
 
 scrollAnim
+.to('.block', 1, {width: '100vw', height: '100vh'})
+.to('.block', 1, {borderRadius: 0,})
+// .to('.block', 1, {backgroundColor: 'green'})
 // .set('.slide', {y: '-100vh'})
 // .set('.slide.one', {width: 0})
-.to('.slide.one', 2, {y: '-=100vh'})
+// .to('.slide.one', 2, {y: '-=100vh'})
 
 new ScrollMagic.Scene({
   triggerElement: '.scroll-trigger',
   // offset: 1,
   triggerHook: 0,
-  duration: '100%',
+  duration: '200%',
 })
 .setTween(scrollAnim)
-.setPin('.scroll-trigger')
+.setPin('.one')
 .addIndicators()
 .addTo(controller)
