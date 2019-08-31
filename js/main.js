81e2b1a4f9e7dfb67309b40fe772db0d8f74fba7
MiguelGarcia95 <mgarcia95951@gmail.com>
2019-08-30 23:57:03 -0500

    create base styling

--- js/main.js
+++ js/main.js
@@ -3,8 +3,7 @@ var controller = new ScrollMagic.Controller();
 // var controller = new ScrollMagic.Controller({vertical: false});
 
 scrollAnim
-.to('.block', 1, {width: '100vw', height: '100vh'})
-.to('.block', 1, {borderRadius: 0,})
+.to('.shadow-01', 1, {top: '+=90%', boxShadow: 'inset 0 0 15px 5px rgba(0,0,0,0.1)', transform: 'rotate(90deg)'})
 // .to('.block', 1, {backgroundColor: 'green'})
 // .set('.slide', {y: '-100vh'})
 // .set('.slide.one', {width: 0})
