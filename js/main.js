695b269e2557df2e0164769405780acf561f3030
MiguelGarcia95 <mgarcia95951@gmail.com>
2019-08-31 15:48:39 -0500

    create second section

--- js/main.js
+++ js/main.js
@@ -5,12 +5,17 @@ var controller = new ScrollMagic.Controller();
 scrollAnim
 .to('.shadow', 1, {top: '100%',})
 .to('.title-decal', 1, {boxShadow: '-50px -50px 5px 0px rgba(0,0,0,0.2)'}, '-=1')
-// .to('.title h1', 0.5, {textShadow: '10px -20px 1px rgba(0,0,0,0.2)' }, '-=1')
+.to('.tr', 0.5, {transform: 'rotate(0deg)', width: '100%'})
+.to('.tl', 0.5, {transform: 'rotate(90deg)', width: '43px', top: '3px'}, '-=0.5')
+.to('.bl', 0.5, {transform: 'rotate(0deg)', width: '100%'}, '-=0.5')
+.to('.br', 0.5, {transform: 'rotate(90deg)', width: '43px', bottom: '3px'}, '-=0.5')
+.to('.logo p', 0.5, {color: '#232323'}, '-=0.5')
+.to('.logo div', 0.5, {backgroundColor: '#232323'}, '-=0.5')
 
 var sceneOne = new ScrollMagic.Scene({
   triggerElement: '.one',
   triggerHook: 0,
-  duration: '200%',
+  duration: '100%',
 })
 .setTween(scrollAnim)
 .setPin('.title')
